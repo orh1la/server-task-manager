71bee5f68fe1975dfa0da74c1b2b47ad6c9d6d0f
ORH1LA <email@domain.com>
2025-12-14 09:47:04 -0500

    Initial commit

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
+import os
 from flask import Flask, jsonify, render_template
 import psutil
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/api/status")
+@app.route('/api/status')
 def status():
     return jsonify({
-        "cpu": psutil.cpu_percent(interval=1),
+        "cpu": psutil.cpu_percent(interval=0.5),
         "ram": psutil.virtual_memory().percent,
         "disk": psutil.disk_usage("/").percent
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
